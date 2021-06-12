52432bb90e7d3fcbad5596047a332f5cf54a2cd9
ansark88 <ansark88@gmail.com>
2021-06-13 00:36:50 +0900

    取得したprofileで画面を置き換えるようにした

--- assets/js/profile.jsx
+++ assets/js/profile.jsx
@@ -1,23 +1,30 @@
 function App() {
     return (
-        <div>
-            <h1>profile</h1>
+        <div id="main">
+            <h1>Hello Profile!</h1>
             <ProfileList />
         </div>
     );
 }
 
+
 class ProfileList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            profiles: []
+            profiles: [],
+            is_active: false,
+            current_profile: {
+                Name: "",
+                Age: 0,
+                Gender: "",
+                FavoriteFoods: [{ "Food": "ラーメン" }, { "Food": "りんご" }, { "Food": "レモン" }],
+            }
         };
     }
 
     componentDidMount() {
-        axios
-            .get('./Profile')
+        axios.get('./Profile')
             .then(res => {
                 console.log(res.data);
                 this.setState({ profiles: this.state.profiles.concat(res.data) });
@@ -27,14 +34,55 @@ class ProfileList extends React.Component {
             });
     }
 
+    get_profile(name) {
+        axios.get('./Profile/' + name)
+            .then(res => {
+                console.log(res.data);
+                this.setState({ current_profile: res.data });
+                this.setState({ is_active: true });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
     render() {
-        const profile = this.state.profiles;
+        return (
+            <div id="profile">
+                <ul>
+                    {this.state.profiles
+                        .map(profile => (
+                            <li key={profile.Name}>
+                                <a href="#" onClick={() => this.get_profile(profile.Name)}>{profile.Name}</a>
+                            </li>))
+                    }
+                </ul>
+                <hr />
+                {this.state.is_active && <Profile current={this.state.current_profile} />}
+            </div>
+        );
+    }
+}
+
+class Profile extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        let foods = [];
+        for (const food of this.props.current.FavoriteFoods) {
+            foods.push(food['Food']);
+        }
 
         return (
             <ul>
-                { this.state.profiles.map(profile => <li key={profile.Name}>{profile.Name}</li>)}
+                <li>Name: {this.props.current.Name}</li>
+                <li>Age: {this.props.current.Age}</li>
+                <li>Gender: {this.props.current.Gender}</li>
+                <li>Favorite Foods: [{foods.join(" ")}]</li>
             </ul>
-        )
+        );
     }
 }
 
